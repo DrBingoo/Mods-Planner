2fc162a7d7d4e8a389ab8798e6c9680970d347db
DrBingoo <94155029+DrBingoo@users.noreply.github.com>
2026-05-31 17:59:23 +0800

    Setup server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const session = require('express-session')
+
+const server = express()
+
+dotenv.config()
+
+server.use(express.static('public'))
+server.set('view engine', 'ejs')
+server.use(express.urlencoded({ extended: true }))
+server.use(express.json())
+server.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 24 * 60 * 60 * 1000 }
+}))
+
+async function connectdbStartServer(){
+    try{
+        await mongoose.connect(process.env.DB)
+        console.log('DB connected')
+        server.listen(5000, 'localhost', () => {
+            console.log('server connected')
+        })
+    }catch(error){
+        console.log(error.message)
+    }
+}
+
+connectdbStartServer()
